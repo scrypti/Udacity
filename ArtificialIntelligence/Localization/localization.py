fe73a66ecf00ebc23881344fef66b22f799bf5ea
Domi <scrypti@gmx.de>
2016-07-29 01:17:09 +0200

    Localization
    setup

---
+++ ArtificialIntelligence/Localization/localization.py
@@ -0,0 +1,7 @@
+# https://classroom.udacity.com/courses/cs373/lessons/48739381/concepts/487350240923#
+
+p = [0.2, 0.2, 0.2, 0.2, 0.2]
+world = ['green', 'red', 'red', 'green', 'green']
+z = 'red'  # sensed value
+pHit = 0.6
+pMiss = 0.2
